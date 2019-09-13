1d4a31435a0aa6765f6459fba550e8942be6ce99
Ryan Everett <ryaneverett33@outlook.com>
2019-09-13 11:37:54 -0400

    Update StripType definitions from rpi-ws281x/rpi-ws281x-powershell
    Adds definitions from PR 4.

--- src/rpi_ws281x/StripType.cs
+++ src/rpi_ws281x/StripType.cs
@@ -11,6 +11,66 @@ namespace rpi_ws281x
     public enum StripType
     {
 		Unknown = 0,
-		WS2812_STRIP = 0x00081000
+		WS2812_STRIP = 0x00081000,
+		
+		/// <summary>
+		/// SK6812_STRIP_RGBW
+		/// </summary>
+		SK6812_STRIP_RGBW = 0x18100800,
+
+		/// <summary>
+		/// SK6812_STRIP_RBGW
+		/// </summary>
+		SK6812_STRIP_RBGW = 0x18100008,
+
+		/// <summary>
+		/// SK6812_STRIP_GRBW
+		/// </summary>
+		SK6812_STRIP_GRBW = 0x18081000,
+
+		/// <summary>
+		/// SK6812_STRIP_GBRW
+		/// </summary>
+		SK6812_STRIP_GBRW = 0x18080010,
+
+		/// <summary>
+		/// SK6812_STRIP_BRGW
+		/// </summary>
+		SK6812_STRIP_BRGW = 0x18001008,
+
+		/// <summary>
+		/// SK6812_STRIP_BGRW
+		/// </summary>
+		SK6812_STRIP_BGRW = 0x18000810,
+
+		/// <summary>
+		/// WS2811_STRIP_RGB
+		/// </summary>
+		WS2811_STRIP_RGB = 0x00100800,
+
+		/// <summary>
+		/// WS2811_STRIP_RBG
+		/// </summary>
+		WS2811_STRIP_RBG = 0x00100008,
+
+		/// <summary>
+		/// WS2811_STRIP_GRB
+		/// </summary>
+		WS2811_STRIP_GRB = 0x00081000,
+
+		/// <summary>
+		/// WS2811_STRIP_GBR
+		/// </summary>
+		WS2811_STRIP_GBR = 0x00080010,
+
+		/// <summary>
+		/// WS2811_STRIP_BRG
+		/// </summary>
+		WS2811_STRIP_BRG = 0x00001008,
+
+		/// <summary>
+		/// WS2811_STRIP_BGR
+		/// </summary>
+		WS2811_STRIP_BGR = 0x00000810,
 	}
 }
